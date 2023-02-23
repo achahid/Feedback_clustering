337cf176832ea6e078ccbc1637c3a53c36327f7a
abdelhak.chahid <ch_abdelhak@hotmail.com>
2023-02-23 12:52:48 +0100

    adjust 1

--- feedback_app.py
+++ feedback_app.py
@@ -23,7 +23,7 @@ from deep_translator import GoogleTranslator
 import sys
 # import huggingface_hub.snapshot_download
 from huggingface_hub import snapshot_download
-
+from sklearn.cluster import AgglomerativeClustering
 from nltk.stem import WordNetLemmatizer
 wnl = WordNetLemmatizer()
 import re
@@ -95,8 +95,8 @@ def data_preprocessing(df):
             df["keyword"] = df["keyword"].apply(lambda x: 'digit-' + x if x.isdigit() else x)
             # print("The keywords are in the process of being translated to ENGLISH. Please hold on ... ")
             my_list = df["keyword"].to_list()
-            df["Keyword_eng"] = translate_to_english(my_list)
-            # df["keyword_eng"] = df["keyword"].apply(lambda x: GoogleTranslator(source='auto', target='en').translate(x))
+            # df["Keyword_eng"] = translate_to_english(my_list)
+            df["keyword_eng"] = df["keyword"].apply(lambda x: GoogleTranslator(source='auto', target='en').translate(x))
             # remove the added prefix from the rows
             df["keyword_eng"] = df["keyword_eng"].apply(lambda x: x.replace("digit-", "") if x.startswith("digit-") else x)
             df["keyword"] = df["keyword"].apply(lambda x: x.replace("digit-", "") if x.startswith("digit-") else x)
@@ -183,6 +183,44 @@ def dfs_xlsx(data_list):
     return processed_data
 
 
+def AgglomerativeClustering_algo(model_name_topics,keywords_df):
+
+    dic={}
+    feedback_list = keywords_df.keyword_eng.to_list()
+    embedder = SentenceTransformer(model_name_topics)
+    corpus_embeddings = embedder.encode(feedback_list)
+
+    # Normalize the embeddings to unit length
+    corpus_embeddings = corpus_embeddings / np.linalg.norm(corpus_embeddings, axis=1, keepdims=True)
+
+    # Perform kmean clustering
+    clustering_model = AgglomerativeClustering(n_clusters=None,
+                                               distance_threshold=1.5)  # , affinity='cosine', linkage='average', distance_threshold=0.4)
+    clustering_model.fit(corpus_embeddings)
+    cluster_assignment = clustering_model.labels_
+
+    clustered_sentences = {}
+    for sentence_id, cluster_id in enumerate(cluster_assignment):
+        if cluster_id not in clustered_sentences:
+            clustered_sentences[cluster_id] = []
+
+        clustered_sentences[cluster_id].append(feedback_list[sentence_id])
+
+    your_df_from_dict = pd.DataFrame.from_dict(clustered_sentences, orient='index')
+    dft = your_df_from_dict.transpose()
+
+    df_results = pd.melt(dft, value_vars=dft.columns)
+    df_results.dropna(inplace=True)
+    df_results.rename(columns={'variable': 'clusters', 'value': 'feedback'}, inplace=True)
+    dic['Aglo_clusters'] = df_results
+    return dic
+
+def option_to_model(level_number,options):
+  try:
+    return options[level_number]
+  except Exception as e:
+    return e
+
 # These are some model options for sentence transformers:f
 option_models = {
     "<select>": '<select>',
@@ -195,9 +233,10 @@ option_models = {
     'Finance':'roberta-base-nli-mean-tokens',
 }
 
+
 html_temp = """
-<div style="background-color:tomato;padding:1.5px">
-<h1 style="color:white;text-align:center;"> KEYWORDS CLUSTERING APP</h1>
+<div style="background-color:blue;padding:1.5px">
+<h1 style="color:white;text-align:center;"> FEEDBACK CLUSTERING APP</h1>
 </div><br>"""
 st.markdown(html_temp,unsafe_allow_html=True)
 
@@ -253,6 +292,7 @@ if st.session_state["authentication_status"]:
 
     if load_K_means:
         long_tail_df, short_tail_df, processed_data = data_preprocessing(keywords_df)
+
         with st.spinner('**The K-MEANS clustering algorithm is currently in operation. Please hold on ...**'):
 
             model_name = 'all-MiniLM-L6-v2'
@@ -262,8 +302,8 @@ if st.session_state["authentication_status"]:
             min_cluster = max(1,np.trunc(max_cluster / 2).astype(int))
             steps = max(1,np.trunc((max_cluster - min_cluster) / 3).astype(int))
 
-            data_list = K_MEANS_TRANSFORMATION(keywords_df, start_cluster=5, end_cluster=15, steps=5)
-            preffix = 'CLUSTER_id_'  # ff
+            data_list = K_MEANS_TRANSFORMATION(processed_data, start_cluster=5, end_cluster=15, steps=5)
+            preffix = 'CLUSTER_id_'
             new_dict = {(preffix + str(key)): value for key, value in data_list.items()}
             data_list = new_dict
 
@@ -280,7 +320,39 @@ if st.session_state["authentication_status"]:
                                data=df_xlsx,
                                file_name='K_MEANS_clustering.xlsx')
 
-    ### TRANSFOMERS :
+    ### AGLOMERATIVE  :
+    model_name = ["<select>", "General Base", "General Roberta", "General miniML_L12", "General miniML_L6",
+                  "Medics", "Education and training", "Finance"]
+
+    select_box = st.selectbox('Select a model Transformer', options=model_name)
+    selected_option = option_to_model(select_box,option_models)
+    load_transformers = st.button('GENERATE CLUSTERS: TRANSFORMERS')
+
+
+    if load_transformers and select_box != '<select>':
+        st.write('You selected model:', selected_option)
+        long_tail_df, short_tail_df, processed_data = data_preprocessing(keywords_df)
+
+        with st.spinner('**The Model Aglomerative clustering algorithm is currently running. Please hold on...**'):
+
+
+            data_list = AgglomerativeClustering_algo(selected_option, processed_data)
+
+            preffix = 'CLUSTER_id_'
+            new_dict = {(preffix + str(key)): value for key, value in data_list.items()}
+            data_list = new_dict
+            df_xlsx = dfs_xlsx(data_list)
+
+            st.write("""
+                        <p style="background-color: #FEC929; color: black; padding: 10px;">
+                        Further examination is recommended for the subsequent clusters..
+                        </p>
+                        """, unsafe_allow_html=True)
+
+            st.subheader("Download data")
+            ste.download_button(label='Download Results',
+                               data=df_xlsx,
+                               file_name='Aglomerative_clustering.xlsx')
 
 
 
